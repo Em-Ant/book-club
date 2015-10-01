f5992437a927cf3962fdcb5a44f8b2a38083b765
EmAnt <joshua999x@gmail.com>
2015-10-01 19:20:33 +0200

    Improve profile

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -4,14 +4,36 @@ angular.module('fullstackApp')
   .controller('SettingsCtrl', function ($scope, User, Auth) {
     $scope.errors = {};
 
-    $scope.panelOption = 'data';
+    $scope.panelOption = 'books';
 
     $scope.update = function(form) {
       $scope.dataSubmitted = true;
       /**
       * TODO: update profile
-      */      
+      */
     };
+
+
+    // DUMMY BOOK LIST
+    $scope.bookInfo = undefined;
+    $scope.yourBooks = [
+      {
+        title: 'The Beauty and the Beast',
+        author: 'John Doe',
+        editor: 'Emant Editions',
+        pages: 123,
+        year: '2004',
+        isbn: '8811999045'
+      }, {
+        thumb: '/assets/images/thumb_0.png',
+        title: 'John the Best',
+        author: 'John Doe',
+        editor: 'Emant Editions',
+        pages: 123,
+        year: '2004',
+        isbn: '8811999045'
+      },
+    ];
 
     $scope.changePassword = function(form) {
       $scope.submitted = true;

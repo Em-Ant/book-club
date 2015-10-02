e7bf11ad3a47398d75aa8aa1bbadeba3f16d5336
EmAnt <joshua999x@gmail.com>
2015-10-02 02:17:19 +0200

    find books on google books, basic CRUD ok

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -1,10 +1,68 @@
 'use strict';
 
 angular.module('fullstackApp')
-  .controller('SettingsCtrl', function ($scope, User, Auth) {
+  .controller('SettingsCtrl', function ($scope, $http, User, Auth) {
     $scope.errors = {};
 
     $scope.panelOption = 'books';
+
+    $scope.saveNewBook = function() {
+      $http.post('/api/books/', $scope.newBook).success(function(res){
+        $scope.newBook = undefined;
+        $scope.yourBooks.push(res);
+      });
+    };
+
+    $scope.getMybooks = function() {
+      $http.get('api/books/mybooks/').success(function(res) {
+        $scope.yourBooks = res;
+      });
+    };
+
+    // Load Books
+    $scope.getMybooks();
+
+    $scope.deleteBook = function(bookId) {
+      $http.delete('api/books/' + bookId).success(function() {
+        $scope.yourBooks = $scope.yourBooks.filter(function(book) {
+          if (book._id === bookId) return false;
+          return true;
+        });
+        console.log($scope.yourBooks);
+      });
+    };
+
+
+    $scope.bookSearch = function(isbn) {
+      var base_url = 'https://www.googleapis.com/books/v1/volumes';
+      $http.jsonp(base_url, {params: {q: 'isbn:' + isbn, callback: 'JSON_CALLBACK'}})
+        .success(function(res) {
+          if (!res.items) return;
+          console.log(res);
+          var newBook = {};
+          var data = res.items[0].volumeInfo;
+          if(data.imageLinks) {
+            newBook.thumb = data.imageLinks.smallThumbnail;
+          }
+          newBook.title = data.title;
+          newBook.authors = data.authors;
+          newBook.pages = data.pageCount;
+          newBook.publisher = data.publisher;
+          newBook.year = data.publishedDate.substr(0,4);
+          if (data.industryIdentifiers) {
+            var idArr = data.industryIdentifiers;
+            idArr.forEach(function(el) {
+              if (el.type === 'ISBN_10') {
+                newBook.isbn_10 = el.identifier;
+              } else if (el.type === 'ISBN_13') {
+                newBook.isbn_13 = el.identifier;               }
+            });
+          }
+          $scope.newBook = newBook;
+          $scope.isbn = undefined;
+        });
+    }
+
 
     $scope.update = function(form) {
       $scope.dataSubmitted = true;
@@ -12,28 +70,6 @@ angular.module('fullstackApp')
       * TODO: update profile
       */
     };
-
-
-    // DUMMY BOOK LIST
-    $scope.bookInfo = undefined;
-    $scope.yourBooks = [
-      {
-        title: 'The Beauty and the Beast',
-        author: 'John Doe',
-        editor: 'Emant Editions',
-        pages: 123,
-        year: '2004',
-        isbn: '8811999045'
-      }, {
-        thumb: '/assets/images/thumb_0.png',
-        title: 'John the Best',
-        author: 'John Doe',
-        editor: 'Emant Editions',
-        pages: 123,
-        year: '2004',
-        isbn: '8811999045'
-      },
-    ];
 
     $scope.changePassword = function(form) {
       $scope.submitted = true;
